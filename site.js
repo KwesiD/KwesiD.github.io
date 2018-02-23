40f9dc7c7f25c38625c53c4346de23dcdb5d2157
KwesiD <kfd233@nyu.edu>
2018-02-23 00:52:00 -0500

    Generated cards. Put some placeholder images. Design, layout, and colors need A LOT of work

---
+++ site.js
@@ -0,0 +1,10 @@
+
+
+// cards = document.getElementsByClassName("pcard");
+// for(let i = 0;i < cards.length;i++){
+// 	cards[i].style.marginTop = (Math.random()*70)+10 + "px";
+// 	cards[i].style.marginBottom = (Math.random()*70)+10 + "px";
+// 	cards[i].style.marginLeft = (Math.random()*70)+10 + "px";
+// 	cards[i].style.marginRight = (Math.random()*70)+10 + "px";	
+
+// }
